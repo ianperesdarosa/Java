e698bebb9b41fee8c33f396a4e19eed8a98a3fae
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-06-24 03:45:32 -0300

    :fire:

---
+++ test/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Msg obj = new Msg();
+        obj.nome = "Ian";
+        obj.idade = 18;
+        obj.msg();
+    }
+}
---
+++ test/src/Msg.java
@@ -0,0 +1,9 @@
+public class Msg {
+    public String nome;
+    public int idade;
+    public void msg(){
+
+        System.out.println("My names is " +this.nome);
+        System.out.println("I have " + this.idade +" years.");
+    }
+}
